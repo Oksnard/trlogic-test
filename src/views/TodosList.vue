c03fd009051a08bdece00f6f0e9639b7d9880a4b
Oksnard <36508223+Oksnard@users.noreply.github.com>
2020-05-05 20:38:21 +0300

    create app starter template

---
+++ src/views/TodosList.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  .home
+    img(
+      alt="Vue logo"
+      src="../assets/logo.png"
+    )
+</template>
+
+<script>
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class TodosList extends Vue {
+  count = 0
+
+  increment() {
+    this.count++
+  }
+
+  decrement() {
+    this.count--
+  }
+}
+</script>
+
+<style lang="stylus">
+.home
+	background pink
+</style>
